46f75b1d3418f0a6f0cbc10fb7143f5dce71c138
Brian Patt <blpatt@mit.edu>
2011-06-14 17:27:37 -0400

    a hack (hopefully) temporary to download all the tour content

--- Projects/Tour/build-scripts/downloadMedia.py
+++ Projects/Tour/build-scripts/downloadMedia.py
@@ -14,6 +14,11 @@ def retrieveAndSave(url):
     else:  
         fullURL = url
 
+    # this is a bit of a hack, probably should remove this (if the server changes)
+    fullURLParts = fullURL.split('/')
+    fullURLParts[-1] = urllib2.quote(fullURLParts[-1])
+    fullURL = '/'.join(fullURLParts)
+
     try:
         response = urllib2.urlopen(fullURL)
     except urllib2.HTTPError:
