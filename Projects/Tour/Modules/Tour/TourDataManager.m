be6a041eca1de4b5c2592a9ae7620a49908ff63d
Alexandra Ellwood <alexandra.ellwood@modolabs.com>
2011-09-20 20:00:11 -0400

    remove preference indicating that we're on a tour when installing a new version

--- Projects/Tour/Modules/Tour/TourDataManager.m
+++ Projects/Tour/Modules/Tour/TourDataManager.m
@@ -50,6 +50,7 @@
     if (![currentTourDataVersion isEqualToString:currentVersion]) {
         // remove old data
         [[CoreDataManager sharedManager] deleteStore];
+        [[NSUserDefaults standardUserDefaults] removeObjectForKey:INITIAL_TOUR_STOP_KEY];
     }
     
     // check if stops already loaded into core data    
